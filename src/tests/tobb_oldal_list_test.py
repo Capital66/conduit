0794eac5b6d98a41649ff373078edde6b1134ba8
Knap Tamás <knaptamas@gmail.com>
2021-08-15 10:02:52 +0200

    CON_TC_005 tobb oldalas lista

---
+++ src/tests/tobb_oldal_list_test.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+import time
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
+def test_webshop():
+    driver.get("http://localhost:1667/#/")
+
+    sign_in = driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[2]/a')
+    sign_in.click()
+
+    email = driver.find_element_by_xpath('//input[@placeholder="Email"]')
+    email.send_keys("testuser3@example.com")
+
+    password = driver.find_element_by_xpath('//input[@placeholder="Password"]')
+    password.send_keys("Abcd123$")
+
+    sign_in_button = driver.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+    sign_in_button.click()
+    time.sleep(3)
+
+    global_feed_button = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[1]/div[1]/ul/li[2]/a')
+    global_feed_button.click()
+
+    page_2_button = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[1]/div[2]/div/div/nav/ul/li[2]/a')
+    page_2_button.click()
+    time.sleep(3)
+
+    element = driver.find_element_by_xpath('//li[@data-test="page-link-2"]')
+    assert element.get_attribute('class') == "page-item active"
+
+    driver.close()
